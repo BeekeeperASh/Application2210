382c09d4c805da7ca7bddbe956019101bdff714b
Beekeeper <alvshinev@gmail.com>
2022-10-22 13:53:09 +0300

    second commit

--- app/src/main/java/com/example/application2210/MainActivity.java
+++ app/src/main/java/com/example/application2210/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Amogubobus");
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editTextTextPersonName);
